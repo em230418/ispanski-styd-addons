319e20d0d78945317e940ba5e6fefdfdca70d712
Eugene Molotov <molotov@it-projects.info>
2023-04-09 09:51:13 +0500

    [ADD] shatatel_razmetki

---
+++ shatatel_razmetki/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Я твою разметку шатал",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "assets": {
+        "web.assets_backend": [
+            "shatatel_razmetki/static/src/css/styles.css",
+        ],
+    }
+}
