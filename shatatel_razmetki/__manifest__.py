c60172908e48f045bc86df7056d5779ec34a3250
Eugene Molotov <molotov@it-projects.info>
2023-04-09 09:57:38 +0500

    [FIX] shatatel_razmetki: хоть какие-то вещи пусть проходят в GitHub Actions

--- shatatel_razmetki/__manifest__.py
+++ shatatel_razmetki/__manifest__.py
@@ -1,10 +1,11 @@
 {
     "name": "Я твою разметку шатал",
     "license": "LGPL-3",
+    "version": "16.0.0.1.0",
     "depends": ["base"],
     "assets": {
         "web.assets_backend": [
             "shatatel_razmetki/static/src/css/styles.css",
         ],
-    }
+    },
 }
